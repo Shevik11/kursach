34d5011d4f875c349cde39e9c3ba87b709a024ff
Shevik11 <maksshevchuk621@gmail.com>
2024-11-16 20:06:25 +0200

    reset project cuz I'm idiot

--- server.py
+++
@@ -1,9 +0,0 @@
-from app import app
-from routes.auth_routes import auth_bp
-from routes.file_routes import file_bp
-
-app.register_blueprint(auth_bp, url_prefix="/auth")
-app.register_blueprint(file_bp, url_prefix="/files")
-
-if __name__ == "__main__":
-    app.run(debug=True)
